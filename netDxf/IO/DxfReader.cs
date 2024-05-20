02d9f60e67d0d28887aaa0af6a93dbf1a279ccc6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-05-20 18:22:26 +0200

    Add new target frameworks support

--- netDxf/IO/DxfReader.cs
+++ netDxf/IO/DxfReader.cs
@@ -165,7 +165,7 @@ namespace netDxf.IO
             if (version < DxfVersion.AutoCad2007)
             {
 
-#if !NET4X
+#if !NET4X && !NETSTANDARD
                 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 #endif
 
