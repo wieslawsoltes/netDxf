2f8bd7851f45b80fa2eb33e8fe49a53414bab0c1
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-09-25 11:00:34 +0200

    Disambiguate shared numeric fixtures from System.Numerics vectors

--- tests/shared/PortableDoubleCases.cs
+++ tests/shared/PortableDoubleCases.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Numerics;
+using BigInteger = System.Numerics.BigInteger;
 using System.Reflection;
 using System.Linq;
 using System.Text;
